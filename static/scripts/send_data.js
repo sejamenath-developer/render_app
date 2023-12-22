a265c568aa4a49b3dc68c268aeb406999a3c5380
sejamenath1 <154619913+sejamenath1@users.noreply.github.com>
2023-12-23 04:50:44 +0530

    Update send_data.js

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -3,7 +3,11 @@ function sendMessage() {
     var result = document.getElementById('results');
     var formData = new FormData();
 
-    formData.append('message', userInput);
+    // Your translation API call would go here
+    // For example, using Google Translate API
+    var translatedMessage = translateToSinhala(userInput); // This function needs implementation
+
+    formData.append('message', translatedMessage);
 
     $.ajax({
         url: '/submit',
@@ -12,9 +16,7 @@ function sendMessage() {
         contentType: false,
         processData: false,
         success: function(data) {
-            // Assuming data.data contains the text to be converted to Sinhala
-            var sinhalaText = new Intl.DisplayNames(['si'], { type: 'language' }).of(data.data);
-            result.innerHTML = '<img src="static/icons/tmp-camera-1703274705850.png" alt="Image Preview" class="result-image" style="width:40px"><br>' + sinhalaText;
+            result.innerHTML = '<img src="static/icons/tmp-camera-1703274705850.png" alt="Image Preview" class="result-image" style="width:40px"><p> Resalt</p><br><br>' + data.data;
             showSuccessMessage();
             
             // Clear the placeholder text after sending the message
