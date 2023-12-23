a90926b78974db18c0eb406f41b9faef0fef7614
sejamenath1 <154619913+sejamenath1@users.noreply.github.com>
2023-12-23 12:10:37 +0530

    Update send_data.js

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -2,8 +2,23 @@ function sendMessage() {
     var userInput = $('#textInput').val();
     var result = document.getElementById('results');
     var formData = new FormData();
+    var progressBar = document.getElementById('progressBar');
+    var progressBarValue = 0;
 
     formData.append('message', userInput);
+
+    // Show the progress bar
+    progressBar.style.display = 'block';
+
+    var interval = setInterval(function() {
+        progressBarValue += 5; // Update the progress bar value (adjust as needed)
+        progressBar.style.width = progressBarValue + '%'; // Update the progress bar width
+
+        if (progressBarValue >= 100) {
+            clearInterval(interval);
+            progressBar.style.display = 'none'; // Hide the progress bar on completion
+        }
+    }, 500); // Adjust the interval duration as needed
 
     $.ajax({
         url: '/submit',
@@ -17,11 +32,6 @@ function sendMessage() {
             
             // Clear the placeholder text after sending the message
             $('#textInput').val('');
-
-            // Additional logic for handling the Python code response if needed
-            // You can access the Python response in the 'data' variable
-            // Example: console.log('Python Response:', data);
-
         },
         error: function(error) {
             console.error('Error:', error);
