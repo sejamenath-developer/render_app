d4c651da22ce3e34937ce7049a06ff9f8623d260
sejamenath1 <154619913+sejamenath1@users.noreply.github.com>
2023-12-23 09:02:06 +0530

    Update send_data.js

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -12,7 +12,7 @@ function sendMessage() {
         contentType: false,
         processData: false,
         success: function(data) {
-            result.innerHTML = '<img src="static/icons/0ad3f9b49bc8456a4a2a1a51fdf0ca4e.gif" alt="Image Preview" class="result-image" style="width:40px"><p><b>Generated text<b></p>' + data.data;
+            result.innerHTML = '<img src="https://i.pinimg.com/originals/a3/89/fb/a389fb3d67ee3836479191686474621f.gif" alt="Image Preview" class="result-image" style="width:40px"><p><b>Generated text<b></p>' + data.data;
             showSuccessMessage();
             
             // Clear the placeholder text after sending the message
