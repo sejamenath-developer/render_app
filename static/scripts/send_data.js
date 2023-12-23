f005c2c27dc64342369592293f17954f57963efe
sejamenath1 <154619913+sejamenath1@users.noreply.github.com>
2023-12-23 13:31:59 +0530

    Update send_data.js

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -10,55 +10,45 @@ function sendMessage() {
     // Show the progress bar
     progressBar.style.display = 'block';
 
-    // This is just an example. Replace this with your actual AJAX request
+    // Interval to update the progress bar
+    var interval = setInterval(function() {
+        progressBarValue += 2; // Adjust the increment value as needed
+        progressBar.style.width = progressBarValue + '%';
+
+        if (progressBarValue >= 100) {
+            clearInterval(interval);
+        }
+    }, 100); // Adjust the interval duration as needed
+
+    // Simulate a delay (replace this with your actual AJAX request)
+    setTimeout(function() {
+        // This simulates a successful response after a delay
+        result.innerHTML = '<br>Generated Resalt  •<br>';
+        showSuccessMessage();
+
+        // Clear the placeholder text after sending the message
+        $('#textInput').val('');
+
+        // Hide the progress bar after receiving the response
+        progressBar.style.display = 'none';
+        progressBar.style.width = '0%'; // Reset progress bar width
+    }, 3000); // Simulated response delay (replace this with your actual AJAX request)
+
+    // This is where you would put your actual AJAX request using $.ajax
+    // Example:
+    /*
     $.ajax({
         url: '/submit',
         type: 'POST',
         data: formData,
         contentType: false,
         processData: false,
-        beforeSend: function() {
-            // Set the expected response time (adjust as needed)
-            var expectedResponseTime = 5000; // in milliseconds
-
-            // Calculate the interval duration based on the expected response time
-            var intervalDuration = expectedResponseTime / 100;
-
-            // Interval to update the progress bar
-            var interval = setInterval(function() {
-                progressBarValue += 1; // Adjust the increment value as needed
-                progressBar.style.width = progressBarValue + '%';
-
-                if (progressBarValue >= 100) {
-                    clearInterval(interval);
-
-                    // Hide the progress bar after receiving the response
-                    progressBar.style.display = 'none';
-                    progressBar.style.width = '0%'; // Reset progress bar width
-                }
-            }, intervalDuration);
-        },
         success: function(data) {
             // Handle the response data
-            result.innerHTML = 'class="result-image" style="width:40px"><h3>   <b>  
-
-Generated Resalt <br>
-•••••••••••••••••••••••
-</b>
-
-</h3><br>' + data.data;
-            showSuccessMessage();
-
-            // Clear the placeholder text after sending the message
-            $('#textInput').val('');
         },
         error: function(error) {
             // Handle error
-            console.error('Error:', error);
-
-            // Hide the progress bar on error
-            progressBar.style.display = 'none';
-            progressBar.style.width = '0%'; // Reset progress bar width
         }
     });
+    */
 }
