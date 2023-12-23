b07ee05490c00b852a2c8e901108c84987d63753
sejamenath1 <154619913+sejamenath1@users.noreply.github.com>
2023-12-23 12:21:36 +0530

    Update send_data.js

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -10,16 +10,17 @@ function sendMessage() {
     // Show the progress bar
     progressBar.style.display = 'block';
 
+    // Interval to update the progress bar
     var interval = setInterval(function() {
-        progressBarValue += 5; // Update the progress bar value (adjust as needed)
-        progressBar.style.width = progressBarValue + '%'; // Update the progress bar width
+        progressBarValue += 1; // Adjust the increment value as needed
+        progressBar.style.width = progressBarValue + '%';
 
         if (progressBarValue >= 100) {
             clearInterval(interval);
-            progressBar.style.display = 'none'; // Hide the progress bar on completion
         }
-    }, 500); // Adjust the interval duration as needed
+    }, 50); // Adjust the interval duration as needed
 
+    // Simulate an AJAX request delay and response time
     $.ajax({
         url: '/submit',
         type: 'POST',
@@ -27,11 +28,18 @@ function sendMessage() {
         contentType: false,
         processData: false,
         success: function(data) {
-            result.innerHTML = '<img src="static/icons/Chatbot.png" alt="Image Preview" class="result-image" style="width:40px"><br>' + data.data;
-            showSuccessMessage();
-            
-            // Clear the placeholder text after sending the message
-            $('#textInput').val('');
+            // Simulate a delay before showing the response
+            setTimeout(function() {
+                result.innerHTML = '<img src="static/icons/Chatbot.png" alt="Image Preview" class="result-image" style="width:40px"><br>Received response!';
+                showSuccessMessage();
+
+                // Clear the placeholder text after sending the message
+                $('#textInput').val('');
+
+                // Hide the progress bar after receiving the response
+                progressBar.style.display = 'none';
+                progressBar.style.width = '0%'; // Reset progress bar width
+            }, 1500); // Simulated response delay (adjust this to match your backend response time)
         },
         error: function(error) {
             console.error('Error:', error);
