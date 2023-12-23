a4f301ba775e3f7533dbdc288d20c4808b26868a
sejamenath1 <154619913+sejamenath1@users.noreply.github.com>
2023-12-23 19:00:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,19 +28,19 @@ generation_config = {
 safety_settings = [
     {
         "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        "threshold": "ALLOW_ALL"
     },
     {
         "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        "threshold": "ALLOW_ALL"
     },
     {
         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        "threshold": "ALLOW_ALL"
     },
     {
         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        "threshold": "ALLOW_ALL"
     }
 ]
 
