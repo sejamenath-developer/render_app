948d06a9cfd80718020224205564373517703ed2
sejamenath1 <154619913+sejamenath1@users.noreply.github.com>
2023-12-23 14:19:36 +0530

    Update send_data.js

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -1,57 +1,26 @@
 function sendMessage() {
-            var userInput = $('#textInput').val();
-            var result = document.getElementById('results');
-            var processingTimeElement = document.getElementById('processingTime');
-            var progressBar = document.getElementById('progressBarInner');
-            var formData = new FormData();
+    var userInput = $('#textInput').val();
+    var result = document.getElementById('results');
+    var formData = new FormData();
 
-            formData.append('message', userInput);
+    formData.append('message', userInput);
 
-            var startTime = performance.now(); // Record start time
-
-            $.ajax({
-                url: '/submit',
-                type: 'POST',
-                data: formData,
-                contentType: false,
-                processData: false,
-                xhr: function () {
-                    var xhr = new window.XMLHttpRequest();
-
-                    // Upload progress
-                    xhr.upload.addEventListener("progress", function (evt) {
-                        if (evt.lengthComputable) {
-                            var percentComplete = (evt.loaded / evt.total) * 100;
-                            progressBar.style.width = percentComplete + '%';
-                        }
-                    }, false);
-
-                    return xhr;
-                },
-                beforeSend: function () {
-                    // Show the progress bar before sending the request
-                    progressBar.style.width = '0%';
-                    progressBar.parentNode.style.display = 'block';
-                },
-                success: function (data) {
-                    var endTime = performance.now(); // Record end time
-                    var processingTime = endTime - startTime; // Calculate processing time
-
-                    result.innerHTML = '<p><b>Generated text<b></p>' + data.data;
-
-                    // Display processing time
-                    processingTimeElement.textContent = 'Processing Time: ' + processingTime.toFixed(2) + ' s';
-
-                    // Clear the placeholder text after sending the message
-                    $('#textInput').val('');
-                },
-                error: function (error) {
-                    console.error('Error:', error);
-                    // Handle error
-                },
-                complete: function () {
-                    // Hide the progress bar when the request is complete
-                    progressBar.parentNode.style.display = 'none';
-                }
-            });
+    $.ajax({
+        url: '/submit',
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function(data) {
+            result.innerHTML = '<h3>• Generated Resalt </h3><br>' + data.data;
+            showSuccessMessage();
+            
+            // Clear the placeholder text after sending the message
+            $('#textInput').val('');
+        },
+        error: function(error) {
+            console.error('Error:', error);
+            // Handle error
         }
+    });
+}
