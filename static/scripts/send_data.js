029f5a5fe516f9a9ce761cd8d94fd374ec2d88b2
sejamenath1 <154619913+sejamenath1@users.noreply.github.com>
2023-12-23 19:58:48 +0530

    Update send_data.js

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -5,6 +5,26 @@ function sendMessage() {
 
     formData.append('message', userInput);
 
+    // Show the progress bar
+    var progressBar = document.getElementById('progressBar');
+    progressBar.style.display = 'block';
+    var width = 1;
+    var interval = setInterval(increaseWidth, 10);
+
+    function increaseWidth() {
+        if (width >= 100) {
+            clearInterval(interval);
+            // Hide the progress bar after completion
+            progressBar.style.display = 'none';
+            
+            // Clear the placeholder text after sending the message
+            $('#textInput').val('');
+        } else {
+            width++;
+            progressBar.style.width = width + '%';
+        }
+    }
+
     $.ajax({
         url: '/submit',
         type: 'POST',
@@ -14,9 +34,6 @@ function sendMessage() {
         success: function(data) {
             result.innerHTML = '<h3>® Description generated</h3><br>' + data.data;
             showSuccessMessage();
-            
-            // Clear the placeholder text after sending the message
-            $('#textInput').val('');
         },
         error: function(error) {
             console.error('Error:', error);
