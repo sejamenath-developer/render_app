02cbc3af6b8e8e8138d0016189c08b68bff3d126
Smenath.development <154619913+sejamenath-developer@users.noreply.github.com>
2023-12-28 21:08:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
 from flask import Flask, render_template, request, jsonify
 from pathlib import Path
-from google.cloud import translate_v2 as translate
+from googletrans import Translator  # Import the Translator from googletrans library
 import google.generativeai as genai
 import mimetypes
 
-# Initialize Flask app, Google Generative AI, and Google Translation API
-app = Flask(__name__)
-genai.configure(api_key="AIzaSyAgYD9komBIepaqDvKT3FJSVbynsc9WVkg")
-translator = translate.Client()
+file_path = ''
 
-# ... (Rest of your existing code remains the same)
+app = Flask(__name__)
+
+# Configure Google Generative AI
+genai.configure(api_key="AIzaSyAgYD9komBIepaqDvKT3FJSVbynsc9WVkg")
+
+# Set up the model, safety settings, etc. (your existing code)
+
+translator = Translator()
+
+@app.route('/')
+def index():
+    return render_template('interface.html')
+
+# ... (your existing code for file upload and submission)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     user_message = request.form.get('message')
     print('Received message from frontend:', user_message)
 
-    # Translate the user message from Sinhala to English
-    translated_message = translator.translate(user_message, source_language='si', target_language='en')['translatedText']
-    
     if not file_path.exists():
         return jsonify({"error": "Could not find the uploaded image"})
 
-    # Determine the MIME type using the mimetypes module
     mime_type, _ = mimetypes.guess_type(file_path)
 
     image_parts = [
@@ -32,8 +38,11 @@ def submit():
         },
     ]
 
+    # Translate input message from Sinhala to English
+    translated_message = translator.translate(user_message, src='si', dest='en').text
+
     prompt_parts = [
-        translated_message,  # Use the translated message here
+        translated_message,  # Use the translated message
         image_parts[0],
         "",
     ]
@@ -41,10 +50,10 @@ def submit():
     response = model.generate_content(prompt_parts)
 
     # Translate the generated response from English back to Sinhala
-    translated_response = translator.translate(response.text, source_language='en', target_language='si')['translatedText']
-    
+    translated_response = translator.translate(response.text, src='en', dest='si').text
+
     print(translated_response)
-    return jsonify(data=translated_response)  # Return the translated response
+    return jsonify(data=translated_response)
 
 if __name__ == '__main__':
     app.run()
